318ee66774ef0141de4abe50b2ef7edbb59b7957
Muhamad Fariz Warman <farizwarman@gmail.com>
2024-04-15 21:54:22 +0700

    default: menambahkan search bar

--- event-management-platform/src/app/pages/navbar.tsx
+++ event-management-platform/src/app/pages/navbar.tsx
@@ -56,7 +56,7 @@ const Navbar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-blue-900">
       <div>
-        <h1 className="text-5xl font-signature ml-2">
+        <h1 className="text-3xl font-signature ml-2">
           <a
             className="link-underline link-underline-black"
             href="/"
@@ -66,58 +66,77 @@ const Navbar = () => {
           </a>
         </h1>
       </div>
-
-      <ul className="hidden md:flex">
-        {links.map(({ id, link }) => (
-          <li
-            key={id}
-            className="nav-links px-4 cursor-pointer capitalize font-medium text-white-500 hover:scale-105 hover:text-white duration-200 link-underline"
-          >
-            <Link href={link}>{link}</Link>
-          </li>
-        ))}
-      </ul>
-      <div className="flex items-center justify-center min-h-screen">
-        <form action="" className="relative mx-auto flex">
-          <input
-            type="search"
-            className="text-xs peer cursor-pointer relative z-10 h-8 w-10 rounded-lg border bg-transparent  pr-6 outline-none focus:rounded-r-none focus:w-full focus:cursor-text focus:border-taupeGray focus:px-3"
-            placeholder="Events..."
-          />
-          <button className="absolute top-0 right-0 bottom-0 my-auto h-8 w-10 px-3 bg-slate-300 rounded-lg peer-focus:relative peer-focus:rounded-l-none">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              x="10px"
-              y="0px"
-              width="20"
-              height="20"
-              viewBox="8 0 40 50"
+      <div>
+        <ul className="hidden md:flex">
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="nav-links px-4 cursor-pointer capitalize font-medium text-white-500 hover:scale-105 hover:text-white duration-200 link-underline"
             >
-              <path d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z"></path>
+              <Link href={link}>{link}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <form className="flex items-center">
+          <label htmlFor="voice-search" className="sr-only">
+            Search
+          </label>
+          <div className="relative w-full">
+            <div className="flex absolute pointer-events-none"></div>
+            <input
+              type="text"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Events near you.."
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-blue-700 border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 rounded-full"
+          >
+            <svg
+              aria-hidden="true"
+              className="mr-2 -ml-1 w-5 h-5"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
             </svg>
+            Search
           </button>
         </form>
       </div>
-      <ul className="hidden md:flex">
-        {links1.map(({ id, link }) => (
-          <li
-            key={id}
-            className=" px-4 cursor-pointer capitalize font-medium text-black hover:scale-105 hover:text-black duration-200 link-underline bg-green-400 rounded-full p-2 mr-1"
-          >
-            <Link href={link}>{link}</Link>
-          </li>
-        ))}
-      </ul>
-      <ul className="hidden md:flex">
-        {links2.map(({ id, link }) => (
-          <li
-            key={id}
-            className=" px-4 cursor-pointer capitalize font-medium text-black hover:scale-105 hover:text-black duration-200 link-underline bg-gray-100 rounded-full p-2 -ml-52"
-          >
-            <Link href={link}>{link}</Link>
-          </li>
-        ))}
-      </ul>
+      <div className="flex items-center justify-center min-h-screen">
+        <ul className="hidden md:flex">
+          {links1.map(({ id, link }) => (
+            <li
+              key={id}
+              className=" px-4 cursor-pointer capitalize font-medium text-black hover:scale-105 hover:text-black duration-200 link-underline bg-green-400 rounded-full p-2 mr-2"
+            >
+              <Link href={link}>{link}</Link>
+            </li>
+          ))}
+        </ul>
+        <ul className="hidden md:flex">
+          {links2.map(({ id, link }) => (
+            <li
+              key={id}
+              className=" px-4 cursor-pointer capitalize font-medium text-black hover:scale-105 hover:text-black duration-200 link-underline bg-gray-100 rounded-full p-2"
+            >
+              <Link href={link}>{link}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
 
       <div
         onClick={() => setNav(!nav)}
@@ -128,22 +147,23 @@ const Navbar = () => {
 
       {nav && (
         <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-white-500">
-          {links.map(({ id, link }) => (
-            <li
-              key={id}
-              className="px-4 cursor-pointer capitalize py-6 text-2xl"
-            >
-              <Link onClick={() => setNav(!nav)} href={link}>
-                {link}
-              </Link>
-            </li>
-          ))}
-          <br />
-          <div className="text-center outline rounded-2xl p-5">
+          <div className="text-center outline rounded-xl px-16">
+            {links.map(({ id, link }) => (
+              <li
+                key={id}
+                className="px-4 cursor-pointer capitalize py-6 text-2xl hover:scale-105 hover:text-white duration-200 link-underline"
+              >
+                <Link onClick={() => setNav(!nav)} href={link}>
+                  {link}
+                </Link>
+              </li>
+            ))}
+          </div>
+          <div className="text-center mt-10 flex">
             {links1.map(({ id, link }) => (
               <li
                 key={id}
-                className="px-6 cursor-pointer capitalize py-2 text-2xl bg-green-800 rounded-md"
+                className="px-6 cursor-pointer capitalize py-2 text-2xl bg-green-500 rounded-md hover:scale-105 hover:text-white duration-200 link-underline"
               >
                 <Link onClick={() => setNav(!nav)} href={link}>
                   {link}
@@ -153,7 +173,7 @@ const Navbar = () => {
             {links2.map(({ id, link }) => (
               <li
                 key={id}
-                className="px-6 cursor-pointer capitalize py-2 text-2xl bg-gray-500 rounded-md my-5"
+                className="px-6 cursor-pointer capitalize py-2 text-2xl bg-gray-500 rounded-md ml-2 hover:scale-105 hover:text-white duration-200 link-underline "
               >
                 <Link onClick={() => setNav(!nav)} href={link}>
                   {link}
