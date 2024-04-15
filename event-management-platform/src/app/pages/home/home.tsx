085bdaa5cb6e28fe5b2edaf0aa743b514ace3b71
Muhamad Fariz Warman <farizwarman@gmail.com>
2024-04-15 14:14:58 +0700

    finalisasi navbar

--- event-management-platform/src/app/pages/home/home.tsx
+++ event-management-platform/src/app/pages/home/home.tsx
@@ -11,54 +11,6 @@ const LandingPage: React.FC = () => {
           We help you find any Events based on your location and book the ticket
           for you so you don't missed out
         </p>
-        <div className=" flex justify-center items-center px-20 my-5">
-          <div className="space-y-0">
-            <div className="flex items-center p-6 space-x-6 bg-white rounded-xl shadow-lg hover:shadow-xl transform hover:scale-105 transition duration-500">
-              <div className="flex bg-gray-100 p-4 w-72 space-x-4 rounded-lg">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 opacity-30"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                  />
-                </svg>
-                <input
-                  className="bg-gray-100 outline-none"
-                  type="text"
-                  placeholder="Article name or keyword..."
-                />
-              </div>
-              <div className="flex py-3 px-4 rounded-lg text-gray-500 font-semibold cursor-pointer">
-                <span>Choose Country</span>
-
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M19 9l-7 7-7-7"
-                  />
-                </svg>
-              </div>
-              <div className="bg-blue-800 py-3 px-5 text-white font-semibold rounded-lg hover:shadow-lg transition duration-3000 cursor-pointer">
-                <span>Search</span>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
     </section>
   );
--- event-management-platform/src/app/pages/navbar.tsx
+++ event-management-platform/src/app/pages/navbar.tsx
@@ -19,15 +19,24 @@ const Navbar = () => {
     },
     {
       id: 3,
-      link: "portfolio",
-    },
-    {
-      id: 4,
-      link: "experience",
+      link: "Explore",
     },
     {
       id: 5,
-      link: "contact",
+      link: "Contact us",
+    },
+  ];
+
+  const links1 = [
+    {
+      id: 1,
+      link: "Login",
+    },
+  ];
+  const links2 = [
+    {
+      id: 1,
+      link: "Sign Up",
     },
   ];
 
@@ -45,7 +54,7 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black">
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-blue-900">
       <div>
         <h1 className="text-5xl font-signature ml-2">
           <a
@@ -53,7 +62,7 @@ const Navbar = () => {
             href="/"
             rel="home"
           >
-            aE<span className="text-blue-800">.</span>
+            aE<span className="text-blue-400">.</span>
           </a>
         </h1>
       </div>
@@ -62,7 +71,48 @@ const Navbar = () => {
         {links.map(({ id, link }) => (
           <li
             key={id}
-            className="nav-links px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 hover:text-white duration-200 link-underline"
+            className="nav-links px-4 cursor-pointer capitalize font-medium text-white-500 hover:scale-105 hover:text-white duration-200 link-underline"
+          >
+            <Link href={link}>{link}</Link>
+          </li>
+        ))}
+      </ul>
+      <div className="flex items-center justify-center min-h-screen">
+        <form action="" className="relative mx-auto flex">
+          <input
+            type="search"
+            className="text-xs peer cursor-pointer relative z-10 h-8 w-10 rounded-lg border bg-transparent  pr-6 outline-none focus:rounded-r-none focus:w-full focus:cursor-text focus:border-taupeGray focus:px-3"
+            placeholder="Events..."
+          />
+          <button className="absolute top-0 right-0 bottom-0 my-auto h-8 w-10 px-3 bg-slate-300 rounded-lg peer-focus:relative peer-focus:rounded-l-none">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="10px"
+              y="0px"
+              width="20"
+              height="20"
+              viewBox="8 0 40 50"
+            >
+              <path d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z"></path>
+            </svg>
+          </button>
+        </form>
+      </div>
+      <ul className="hidden md:flex">
+        {links1.map(({ id, link }) => (
+          <li
+            key={id}
+            className=" px-4 cursor-pointer capitalize font-medium text-black hover:scale-105 hover:text-black duration-200 link-underline bg-green-400 rounded-full p-2 mr-1"
+          >
+            <Link href={link}>{link}</Link>
+          </li>
+        ))}
+      </ul>
+      <ul className="hidden md:flex">
+        {links2.map(({ id, link }) => (
+          <li
+            key={id}
+            className=" px-4 cursor-pointer capitalize font-medium text-black hover:scale-105 hover:text-black duration-200 link-underline bg-gray-100 rounded-full p-2 -ml-52"
           >
             <Link href={link}>{link}</Link>
           </li>
@@ -71,23 +121,46 @@ const Navbar = () => {
 
       <div
         onClick={() => setNav(!nav)}
-        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+        className="cursor-pointer pr-4 z-10 text-white-500 md:hidden"
       >
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
 
       {nav && (
-        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-white-500">
           {links.map(({ id, link }) => (
             <li
               key={id}
-              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+              className="px-4 cursor-pointer capitalize py-6 text-2xl"
             >
               <Link onClick={() => setNav(!nav)} href={link}>
                 {link}
               </Link>
             </li>
           ))}
+          <br />
+          <div className="text-center outline rounded-2xl p-5">
+            {links1.map(({ id, link }) => (
+              <li
+                key={id}
+                className="px-6 cursor-pointer capitalize py-2 text-2xl bg-green-800 rounded-md"
+              >
+                <Link onClick={() => setNav(!nav)} href={link}>
+                  {link}
+                </Link>
+              </li>
+            ))}
+            {links2.map(({ id, link }) => (
+              <li
+                key={id}
+                className="px-6 cursor-pointer capitalize py-2 text-2xl bg-gray-500 rounded-md my-5"
+              >
+                <Link onClick={() => setNav(!nav)} href={link}>
+                  {link}
+                </Link>
+              </li>
+            ))}
+          </div>
         </ul>
       )}
     </div>
