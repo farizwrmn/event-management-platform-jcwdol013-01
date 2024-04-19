5a3b164bb2bdaf29d52b6cd939270302b0dc869c
Muhamad Fariz Warman <farizwarman@gmail.com>
2024-04-19 14:12:00 +0700

    finalisasi navbar

--- event-management-platform/src/app/pages/navbar.tsx
+++ event-management-platform/src/app/pages/navbar.tsx
@@ -67,14 +67,55 @@ const Navbar = () => {
           ))}
         </ul>
       </div>
+      <div className="cursor-pointer text-white-500 invisible xl:visible">
+        <form className="flex items-center">
+          <label htmlFor="voice-search" className="sr-only">
+            Search
+          </label>
+          <div className="relative w-full">
+            <div className="flex absolute pointer-events-none"></div>
+            <input
+              type="search"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-1  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Events near you.."
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="inline-flex items-center py-2 px-2 ml-2 text-xs font-medium text-white bg-blue-700 border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 rounded-full"
+          >
+            <svg
+              aria-hidden="true"
+              className="w-5 h-5"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="3"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+          </button>
+        </form>
+      </div>
+      <div className="xl:m-10">
+        <UserButton afterSignOutUrl="/" />
+      </div>
       <SignedOut>
-        <Button asChild className="rounded-full" size="lg" variant="secondary">
+        <Button
+          asChild
+          className="rounded-full flex xl:m-5"
+          variant="secondary"
+          size="sm"
+        >
           <Link href="/sign-in">Login</Link>
         </Button>
       </SignedOut>
-      <div className="flex">
-        <UserButton afterSignOutUrl="/" />
-      </div>
       <div
         onClick={() => setNav(!nav)}
         className="cursor-pointer pr-0 z-10 text-white-500 md:hidden"
