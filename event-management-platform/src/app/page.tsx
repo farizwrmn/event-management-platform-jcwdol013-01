0b6306755bf065cf5baa9fd25207955a507b28b2
Muhamad Fariz Warman <farizwarman@gmail.com>
2024-04-23 17:41:21 +0700

    layout event homepage

--- event-management-platform/src/app/page.tsx
+++ event-management-platform/src/app/page.tsx
@@ -1,9 +1,11 @@
+import CardBlog from "./pages/home/EventCard";
 import Hero from "./pages/home/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <CardBlog />
     </>
   );
 }
