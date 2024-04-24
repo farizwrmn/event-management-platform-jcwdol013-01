4bf33581af7b0f388b8ce9c5c7927b55e2fdc044
Muhamad Fariz Warman <farizwarman@gmail.com>
2024-04-24 21:47:21 +0700

    banner promo

--- event-management-platform/src/app/page.tsx
+++ event-management-platform/src/app/page.tsx
@@ -1,11 +1,13 @@
 import CardBlog from "./pages/home/EventCard";
 import Hero from "./pages/home/Hero";
+import BannerPromo from "./pages/home/BannerPromo";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <CardBlog />
+      <BannerPromo />
     </>
   );
 }
