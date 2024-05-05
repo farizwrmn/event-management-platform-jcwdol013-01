f127a5d96634db20deb3ee5428712bf6944d41cf
Muhamad Fariz Warman <farizwarman@gmail.com>
2024-05-05 13:18:20 +0700

    search debounce: eventPage, category query: explorePage

---
+++ event-management-platform/src/app/Explore/page.tsx
@@ -0,0 +1,35 @@
+import CategoryFilter from "@/components/shared/CategoryFilter";
+import Collection from "@/components/shared/Collection";
+import { getAllEvents } from "@/lib/actions/event.action";
+import { SearchParamProps } from "@/types";
+
+const Explore = async ({ searchParams }: SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || "";
+  const category = (searchParams?.category as string) || "";
+  const events = await getAllEvents({
+    query: searchText,
+    category,
+    page,
+    limit: 20,
+  });
+  return (
+    <>
+      <div className="p-5 text-center lg:text-start border-b flex mb-10 lg:w-[260px] wrapper items-center">
+        <h2 className="h2-bold">Explore</h2>
+        <CategoryFilter />
+      </div>
+      <Collection
+        data={events?.data}
+        emptyTitle="No Events Found."
+        emptyStateSubtext="Come back later"
+        collectionType="All_Events"
+        limit={4}
+        page={1}
+        totalPages={1}
+      />
+    </>
+  );
+};
+
+export default Explore;
